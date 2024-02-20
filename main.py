c112f41433be5209967c6dede2b1a61d9b3d2025
merrivoir <merrivoir@gmail.com>
2024-02-20 09:36:31 +0600

    Добавление логирования

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from datetime import datetime
 import time
 from config import *
 from generate_time_images import *
+import logging
+
+logging.basicConfig(level=logging.INFO, filename="py_log.log", format="%(asctime)s %(levelname)s %(message)s")
 
 client = TelegramClient('chgTime', api_id, api_hash, system_version="1.1")
 client.start()
@@ -13,11 +16,15 @@ while True:
     photo = client.upload_file(f"time.png")
     try:
         client(UploadProfilePhotoRequest(file=photo))
+        logging.info(f"Фото загружено")
+        time.sleep(300)
+        client(DeletePhotosRequest(client.get_profile_photos('me')))
+        logging.info(f"Фото удалено")
     except errors.FloodWaitError as e:
         print(e.seconds)
+        logging.info(f"Ошибка флуда: необходимо подождать {e.seconds} секунд.")
         time.sleep(e.seconds)
-    time.sleep(60)
-    client(DeletePhotosRequest(client.get_profile_photos('me')))
+    
 
 if __name__ == '__main__':
 	pass
